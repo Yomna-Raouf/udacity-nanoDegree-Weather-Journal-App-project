cd78e84356cdce9d1c7a10a288d865e99805438a
yomna raouf <yomnaraouf99@gmail.com>
2021-02-14 01:28:57 -0800

    Setting the Server Up

--- server.js
+++ server.js
@@ -2,8 +2,12 @@
 projectData = {};
 
 // Require Express to run server and routes
+import express from "express";
+import cors from 'cors';
+import bodyParser from 'body-parser';
 
 // Start up an instance of app
+const app = express();
 
 /* Middleware*/
 //Here we are configuring express to use body-parser as middle-ware.
@@ -11,9 +15,24 @@ app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
 
 // Cors for cross origin allowance
+app.use(cors());
 
 // Initialize the main project folder
 app.use(express.static('website'));
 
+// Initialize all route with a callback function
+
+// Callback function to complete GET '/all'
+
+
+
+// Post Route
 
 // Setup Server
+app.listen(process.env.PORT || 8000, () =>
+  console.log(`Server running on port 8000 or ${process.env.PORT}`)
+);
+
+
+
+
